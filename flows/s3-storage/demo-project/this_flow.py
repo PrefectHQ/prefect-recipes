5e7ad90d2795e84b8d0fe58cf2469ea52b332d22
Nathan Nowack <thrast36@gmail.com>
2021-11-12 16:28:54 -0600

    restructure repo

--- flows/s3-storage/demo-project/this_flow.py
+++ flows/s3-storage/demo-project/this_flow.py
@@ -1,6 +1,8 @@
 from prefect import Flow, task
-from prefect.core.task import Parameter
+# from prefect.core.task import Parameter
 from prefect.storage.s3 import S3
+
+from helpers import custom
 
 # S3 storage
 
@@ -8,22 +10,22 @@ PROJECT = "test-project"
 
 storage = S3(
     bucket = "cs-template-s3-flow-storage",
-    key = f"{PROJECT}/my_import_job",
+    key = f"{PROJECT}/this_flow.py",
     stored_as_script = True,
-    local_script_path = "s3_on_k8s.py",
+    local_script_path = "this_flow.py",
 )
 
 
 @task(name="General import", log_stdout=True)
-def ImportJob(i: str):
-    print('do something')
+def ImportJob(i: object):
+    print(i.info)
 
 with Flow(
     name='Import Some Sources',
     storage=storage,
 
 ) as flow:
-    sources = Parameter('sources', default=['']*5)
+    sources = [custom.MyClass() for i in range(5)]
     ImportJob.map(sources)
 
 if __name__ == "__main__":
