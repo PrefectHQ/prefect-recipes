a3435106761230effc79854966ebedf8f5b37c3c
Nathan Nowack <thrast36@gmail.com>
2021-11-24 14:46:53 -0600

    add whitelist for secret export

--- tools/import-secrets.py
+++ tools/import-secrets.py
@@ -1,15 +1,22 @@
 from prefect import Client
 import toml
 
+# Get prefect client
 client = Client()
 
-CONFIG = 'config.toml' # probably this in practice: '~/.prefect/config.toml'
+CONFIG = '~/.prefect/config.toml'
 
+# all secrets in the config.toml at the CONFIG path
 secrets = toml.load(CONFIG)['context']['secrets']
 
+# the only secret names you want to sync to your cloud tenant
+whitelist = ['SPECIFIC_SECRET_TO_SYNC']
+
+# secret names you don't want to sync to your cloud tenant
 blacklist = ['EXTRA_SECRET_SECRET1', 'EXTRA_SECRET_SECRET2']
 
+# sync desired local secrets to the current cloud tenant
 for n, v in secrets.items():
-    if n not in blacklist:
-        print(f'Setting secret: {n}')
+    if n in whitelist: # OR if n not in blacklist: 
+        print(f'Setting cloud secret: {n}')
         client.set_secret(name=n, value=v)
