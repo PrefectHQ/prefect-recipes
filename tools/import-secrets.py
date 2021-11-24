eaed86127f274e21003be219504e9e076be6ecb1
Nathan Nowack <thrast36@gmail.com>
2021-11-24 14:37:02 -0600

    add blacklist for secret export

--- tools/import-secrets.py
+++ tools/import-secrets.py
@@ -7,6 +7,9 @@ CONFIG = 'config.toml' # probably this in practice: '~/.prefect/config.toml'
 
 secrets = toml.load(CONFIG)['context']['secrets']
 
+blacklist = ['EXTRA_SECRET_SECRET1', 'EXTRA_SECRET_SECRET2']
+
 for n, v in secrets.items():
-    print(f'Setting secret: {n}')
-    client.set_secret(name=n, value=v)
+    if n not in blacklist:
+        print(f'Setting secret: {n}')
+        client.set_secret(name=n, value=v)
