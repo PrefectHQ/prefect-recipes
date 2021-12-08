be329037a39ac50636a12a53f2307ad2d4c91fea
Jamie Dick <jamie@prefect.io>
2021-12-07 21:54:25 -0500

    initial

---
+++ aws/terraform/network/versions.tf
@@ -0,0 +1,6 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    aws = ">= 3.6.0"
+  }
+}
---
+++ aws/terraform/state-management/variables.tf
@@ -0,0 +1,4 @@
+variable "bucket_name" {
+  type        = string
+  description = "unique name to label the S3 bucket"
+}
