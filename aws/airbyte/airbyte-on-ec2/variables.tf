5d06a16151708f7265fb0b9e001c2ddd83f25ef3
Jamie Dick <jamie@prefect.io>
2021-12-17 12:00:11 -0500

    initial

--- aws/airbyte/airbyte-on-ec2/variables.tf
+++ aws/airbyte/airbyte-on-ec2/variables.tf
@@ -1,3 +1,8 @@
+variable "environment" {
+  type        = string
+  description = "SDLC stage"
+  default     = "dev"
+}
 variable "instance_type" {
   type        = string
   description = "AWS instance type, default requirement is t2.medium"
@@ -25,11 +30,26 @@ variable "vpc_id" {
   type        = string
   description = "ID of the VPC to deploy the airbyte instance into"
 }
-variable "subnet_id" {
-  type        = string
-  description = "ID of subnet to deploy airbyte instance into"
+variable "subnet_ids" {
+  type        = list(string)
+  description = "IDs of subnet to deploy airbyte instance into"
 }
 variable "ingress_cidrs" {
   type        = list(string)
   description = "list of cidr ranges to allow ssh access to your airbyte instance"
+}
+variable "min_capacity" {
+  type        = number
+  description = "minimum number of Prefect agents to be running at any given time"
+  default     = 1
+}
+variable "max_capacity" {
+  type        = number
+  description = "maximum number of prefect agents to be running at any given time"
+  default     = 1
+}
+variable "desired_capacity" {
+  type        = number
+  description = "desired number of prefect agents to be running at any given time"
+  default     = 1
 }
