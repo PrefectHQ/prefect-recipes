7f3d9b87b5a051d0ef795a09ce9b1350f1211b80
Jamie Dick <jamie@prefect.io>
2021-12-18 12:01:11 -0500

    reorg

--- aws/airbyte/airbyte-on-ec2/variables.tf
+++ aws/airbyte/airbyte-on-ec2/variables.tf
@@ -1,8 +1,3 @@
-variable "environment" {
-  type        = string
-  description = "SDLC stage"
-  default     = "dev"
-}
 variable "instance_type" {
   type        = string
   description = "AWS instance type, default requirement is t2.medium"
@@ -12,9 +7,37 @@ variable "ami_id" {
   type        = string
   description = "AMI to launch the EC2 instance from"
 }
+variable "environment" {
+  type        = string
+  description = "SDLC stage"
+  default     = "dev"
+}
+variable "vpc_id" {
+  type        = string
+  description = "ID of the VPC to deploy the airbyte instance into"
+}
+variable "subnet_id" {
+  type        = string
+  description = "ID of subnet to deploy airbyte instance into"
+}
+variable "min_capacity" {
+  type        = number
+  description = "minimum number of Airbyte instances to be running at any given time"
+  default     = 1
+}
+variable "max_capacity" {
+  type        = number
+  description = "maximum number of Airbyte instances to be running at any given time"
+  default     = 1
+}
+variable "desired_capacity" {
+  type        = number
+  description = "desired number of Airbyte instances to be running at any given time"
+  default     = 1
+}
 variable "linux_type" {
   type        = string
-  description = "Type of linux instance"
+  description = "type of linux instance"
   default     = "linux_amd64"
 }
 variable "key_name" {
@@ -26,30 +49,7 @@ variable "volume_size" {
   description = "size of volume to attach to airbyte instance; default requirement is 30GB"
   default     = 30
 }
-variable "vpc_id" {
-  type        = string
-  description = "ID of the VPC to deploy the airbyte instance into"
-}
-variable "subnet_ids" {
-  type        = list(string)
-  description = "IDs of subnet to deploy airbyte instance into"
-}
 variable "ingress_cidrs" {
   type        = list(string)
   description = "list of cidr ranges to allow ssh access to your airbyte instance"
-}
-variable "min_capacity" {
-  type        = number
-  description = "minimum number of Prefect agents to be running at any given time"
-  default     = 1
-}
-variable "max_capacity" {
-  type        = number
-  description = "maximum number of prefect agents to be running at any given time"
-  default     = 1
-}
-variable "desired_capacity" {
-  type        = number
-  description = "desired number of prefect agents to be running at any given time"
-  default     = 1
 }
