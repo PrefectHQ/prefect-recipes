a21ca793a6595810a4962132c9568ee18f2478b6
Nathan Nowack <thrast36@gmail.com>
2022-01-10 10:27:57 -0600

    parameterize sync ids

--- prefect/flows/airbyte-orchestration/flow.py
+++ prefect/flows/airbyte-orchestration/flow.py
@@ -1,5 +1,5 @@
 from datetime import timedelta
-from prefect import Flow, mapped
+from prefect import Flow, Parameter
 from prefect.tasks.airbyte.airbyte import AirbyteConnectionTask
 from prefect.tasks.secrets import PrefectSecret
 
@@ -7,13 +7,12 @@ airbyte_sync_task = AirbyteConnectionTask(
     max_retries=3, retry_delay=timedelta(seconds=10)
 )
 
-
 with Flow(
     'airbyte sync',
 
 ) as flow:
 
-    airbyte_sync_ids = ['xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx']
+    airbyte_sync_id = Parameter('airbyte_sync_id', default=None)
 
     host, port = (
         PrefectSecret("AIRBYTE_HOSTNAME"),
@@ -24,7 +23,7 @@ with Flow(
         airbyte_server_host=host,
         airbyte_server_port=port,
         airbyte_api_version="v1",
-        connection_id=mapped(airbyte_sync_ids)
+        connection_id=airbyte_sync_id
     )
 
 if __name__ == "__main__":
