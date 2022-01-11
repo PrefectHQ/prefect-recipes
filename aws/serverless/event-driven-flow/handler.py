c52a767b012aca893482c1d997e879f1ba204668
George Coyne <gabcoyne@gmail.com>
2022-01-11 10:55:45 -0600

    add pre-commit, update formatting

--- aws/serverless/event-driven-flow/handler.py
+++ aws/serverless/event-driven-flow/handler.py
@@ -2,38 +2,43 @@
 # aws lambda-based, event-driven flow trigger
 # using example event: s3:ObjectCreated:*
 
-import boto3
 import logging
 import os
+from typing import Any, Dict
+
+import boto3
+
 import prefect
 from prefect.run_configs.kubernetes import KubernetesRun
-from typing import Any, Dict
 
 logging.basicConfig()
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 # Get secret(s) from AWS Systems Manager Parameter Store
-ssm = boto3.client('ssm')
-PREFECT_AUTH_TOKEN = ssm.get_parameter(Name='keyName')
+ssm = boto3.client("ssm")
+PREFECT_AUTH_TOKEN = ssm.get_parameter(Name="keyName")
 prefect_client = prefect.Client(api_key=PREFECT_AUTH_TOKEN)
+
 
 # kick-off flow run with prefect.Client instance
 def trigger_flow_run(params: Dict[str, Any]) -> dict:
     return prefect_client.create_flow_run(
         parameters=params,
-        version_group_id=os.getenv('PREFECT_VERSION_GROUP_ID'),
+        version_group_id=os.getenv("PREFECT_VERSION_GROUP_ID"),
         run_config=KubernetesRun(),
     )
 
+
 # parse new S3 object as needed for flow params
 def parse(s3_object: object) -> dict:
-    # TODO 
+    # TODO
     return dict()
+
 
 # parse event JSON (e.g. S3 ObjectCreated -> flow params)
 def run(event, context):
-    s3 = boto3.resource('s3')
+    s3 = boto3.resource("s3")
     s3_bucket = event["Records"][0]["s3"]["bucket"]["name"]
     s3_key = event["Records"][0]["s3"]["object"]["key"]
     s3_object = s3.Object(s3_bucket, s3_key)
@@ -42,7 +47,7 @@ def run(event, context):
 
     try:
         trigger_flow_run(params)
-        return {"success": True}    
+        return {"success": True}
 
     except prefect.Client.ClientError:
         raise
