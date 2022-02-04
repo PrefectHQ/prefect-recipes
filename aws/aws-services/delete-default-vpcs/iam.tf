1b8b8c042899ccfb909006fc0e062960a89bf3a5
Jamie Dick <jamie@prefect.io>
2022-02-03 22:48:32 -0500

    tfsec

--- aws/aws-services/delete-default-vpcs/iam.tf
+++ aws/aws-services/delete-default-vpcs/iam.tf
@@ -1,18 +1,15 @@
-resource "aws_iam_role" "lambda_exec" {
-  name               = "${var.function_name}-execution-role-${data.aws_region.current.name}"
-  assume_role_policy = data.aws_iam_policy_document.lambda_exec.json
+resource "aws_iam_role" "lambda" {
+  name_prefix        = "${var.function_name}-execution"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_policy.json
 }
 
-data "aws_iam_policy_document" "lambda_exec" {
+data "aws_iam_policy_document" "lambda_assume_policy" {
   version = "2012-10-17"
 
   statement {
-    sid = "LambdaExecutionRole"
-
+    sid     = "LambdaExecutionRole"
     actions = ["sts:AssumeRole"]
-
-    effect = "Allow"
-
+    effect  = "Allow"
     principals {
       type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
@@ -20,66 +17,55 @@ data "aws_iam_policy_document" "lambda_exec" {
   }
 }
 
-resource "aws_iam_policy" "base_policy" {
-  name        = "lambda-${var.function_name}-policy-${data.aws_region.current.name}"
+resource "aws_iam_policy" "lambda" {
+  name_prefix = "lambda-${var.function_name}"
   description = "IAM policy to allow lambda to delete vpcs across regions"
-
-  policy = data.aws_iam_policy_document.base_policy.json
+  policy      = data.aws_iam_policy_document.lambda.json
 }
 
-data "aws_iam_policy_document" "base_policy" {
+data "aws_iam_policy_document" "lambda" { #tfsec:ignore:aws-iam-no-policy-wildcards
   version = "2012-10-17"
 
   statement {
-    sid = "LambdaAccountPermissions"
-
-    actions = ["ec2:*"]
-
-    effect = "Allow"
-
+    sid       = "LambdaAccountPermissions"
+    actions   = ["ec2:*"]
+    effect    = "Allow"
     resources = ["*"]
   }
 
   statement {
     sid = "AllowCreationOfServiceLinkedRoles"
-
     actions = [
       "iam:CreateServiceLinkedRole"
     ]
-    effect = "Allow"
-
+    effect    = "Allow"
     resources = ["*"]
   }
 
   statement {
     sid = "AllowLambdasAssumeRole"
-
     actions = [
       "sts:AssumeRole"
     ]
-    effect = "Allow"
-
+    effect    = "Allow"
     resources = ["*"]
   }
 
   statement {
     sid = "LambdaLogGroupPermissions"
-
     actions = [
       "logs:CreateLogGroup",
       "logs:CreateLogStream",
       "logs:PutLogEvents"
     ]
-
     effect = "Allow"
-
     resources = [
       "arn:aws:logs:*:*:*"
     ]
   }
 }
 
-resource "aws_iam_role_policy_attachment" "base" {
-  role       = aws_iam_role.lambda_exec.name
-  policy_arn = aws_iam_policy.base_policy.arn
+resource "aws_iam_role_policy_attachment" "lambda" {
+  role       = aws_iam_role.lambda.name
+  policy_arn = aws_iam_policy.lambda.arn
 }
--- aws/aws-services/delete-default-vpcs/main.tf
+++ aws/aws-services/delete-default-vpcs/main.tf
@@ -1,18 +1,17 @@
-resource "aws_lambda_function" "function" {
+resource "aws_lambda_function" "function" { #tfsec:ignore:aws-lambda-enable-tracing
   description   = "Pulls all regions and loops through to remove default VPC and associated resources"
   function_name = var.function_name
-  role          = aws_iam_role.lambda_exec.arn
+  role          = aws_iam_role.lambda.arn
   runtime       = "python3.8"
   handler       = "main.lambda_handler"
   timeout       = 180
   memory_size   = 128
 
-
   filename         = data.archive_file.source.output_path
   source_code_hash = data.archive_file.source.output_base64sha256
 }
 
-resource "aws_cloudwatch_log_group" "lambda_logs" {
+resource "aws_cloudwatch_log_group" "log_group" { #tfsec:ignore:aws-cloudwatch-log-group-customer-key
   name              = "/aws/lambda/${var.function_name}"
   retention_in_days = 30
 }
