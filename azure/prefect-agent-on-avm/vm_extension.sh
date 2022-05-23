0960f52f93c1c67fe4558b75bbc6df55849993db
Chris Boyd <chris.b@prefect.io>
2022-05-23 14:50:10 -0400

    include vm_extension.sh for terraform

---
+++ azure/prefect-agent-on-avm/vm_extension.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#Package updates and installation
+sudo apt-get update -y
+
+#Install pip3
+sudo apt install python3-pip -y
+
+#Install prefect
+python3 -m pip install -U "prefect>=2.0b"
+
+#Create a default work-queue
+/usr/local/bin/prefect work-queue create default
+
+sudo cat << EOF > /etc/systemd/system/prefect-agent.service
+[Unit]
+Description=Prefect Agent Service
+After=network.target
+StartLimitIntervalSec=0
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+User=azureuser
+ExecStart=/usr/local/bin/prefect agent start default
+
+[Install]
+WantedBy=default.target
+EOF
+
+#Reload systemctl to pickup the service
+sudo systemctl daemon-reload
+
+#Enable the service to start on boot
+sudo systemctl enable prefect-service
+
+#Start the service
+sudo systemctl start prefect-service
