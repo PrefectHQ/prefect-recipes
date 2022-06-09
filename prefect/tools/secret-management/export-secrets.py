7538fede1ff6a2f62ca43504af4c8375267c09a2
Nathan Nowack <thrast36@gmail.com>
2022-06-09 10:38:44 -0500

    ran black

--- prefect/tools/secret-management/export-secrets.py
+++ prefect/tools/secret-management/export-secrets.py
@@ -5,18 +5,19 @@ from prefect.client import Client, Secret
 
 
 def get_secret_names(client: Client) -> List[str]:
-    
+
     response = client.graphql(query="query {secretNames}")
-    
-    names = response['data']['secretNames']
-    
+
+    names = response["data"]["secretNames"]
+
     return names
 
 
 def export_current_tenant_secrets() -> None:
     secrets = {i: Secret(i).get() for i in get_secret_names(Client())}
-    with open('secrets.json', 'w') as f:
+    with open("secrets.json", "w") as f:
         f.write(json.dumps(secrets))
+
 
 if __name__ == "__main__":
     export_current_tenant_secrets()
