794f3189be0a652e1dca43bc5e1b3a113a5be551
Chris Boyd <chris.b@prefect.io>
2022-07-06 09:25:31 -0400

    added requirements.txt

--- monitoring/monitor.py
+++ monitoring/monitor.py
@@ -1,8 +1,5 @@
 from prometheus_client import start_http_server, Summary, Gauge
-import random
 import time
-#import flows
-import os
 import prefect
 from prefect import task, Flow
 
@@ -10,32 +7,44 @@ polling_interval_seconds = 30
 prefectGraphQL = "http://127.0.0.1:4200"
 exporter_port = 8000
 
-# all_Projects = []
-# Create a metric to track time spent and requests made.
 client = prefect.Client()
 
-# Prometheus Variables; Type('name', 'Description')
-projectTotal = Gauge('prefect_projects', 'Number of Projects',['name'])
-flowTotal = Gauge('prefect_flows', 'Number of Flows by Project',['project_name', ])
+# Project Variables
+projectNumber = Gauge('project_number', 'Total count of all Prefect Projects')
+projectTotal = Gauge('prefect_projects', 'Number of Projects by Name',['name'])
+# Flow Variables
+
+flowTotal = Gauge('prefect_flows', 'Total of All Flows, All Projects')
+flowProjectTotal = Gauge('prefect_flows_by_project', 'Number of Flows by Project Name',['project_id', 'project_name'])
+flowRunTotal = Gauge('prefect_flowruns_total', 'Number of total flow runs by flow ID', ['project_id', 'project_name'])
+flowRunTotalSuccess = Gauge('prefect_flowruns_success', 'Number of successful flow runs by flow ID', ['project_id', 'project_name'])
+
+# Main loop retrieves exports all metrics
+# Each export queries GraphQL, extracts relevant info, and exports to a metrics
 
 def getAllMetrics():
-    exportProjects()
-    queryAllFlows()
+    allProjects = queryAllProjects()
+    exportAllProjects(allProjects)
+    exportAllFlows()
+    exportFlowsByProject(allProjects)
+    exportFlowRunTotal(allProjects)
+    exportFlowRunSuccess(allProjects)
+
 
 # Queries GraphQL for all projects. Query returns a json object, which is passed to listify.
 @task
 def queryAllProjects() -> list: 
-    r = client.graphql(
-        {
-            'query Projects': {
-                'project': {
-                    'id',
-                    'name',
-                    'tenant_id'
-                }
-            }
+    query = """
+    query Projects {
+        project {
+            id,
+            name,
+            tenant_id,
         }
-    )
+    }
+    """
+    r = client.graphql(query=query)
+    
     projectList = listifyProjects.run(r)
     return projectList
 
@@ -52,40 +61,61 @@ def listifyProjects(all_Projects: object) -> list:
         projectList.append(project)
     return projectList
 
-@task
-def exportProjects():
-    # Updates projectTotal metrics with the label and value of each project queried
-    for project in queryAllProjects.run():
-        projectTotal.labels(project['name']).set(1)
 
 @task
-def queryAllFlows() -> list: 
-    r = client.graphql(
-        {
-            'query Flows': {
-                'flow': {
-                    'id',
-                    'flow_group_id',
-                    'name',
-                    'project_id',
-                    'is_schedule_active'
-                }
+def queryAllFlows() -> int: 
+    query = """
+        query Flows {
+            flow {
+                id,
+                flow_group_id,
+                name,
+                project_id,
+                is_schedule_active
             }
         }
-    )
+    """
+    r = client.graphql(query=query)
+        
+    return len(r['data']['flow'])
 
-    return listifyFlows(r)
+@task
+def listifyFlows(all_Flows: object) -> list:
+    flowList = []
+    for flow in all_Flows['data']['flow']:
+        f = {
+            "id": flow['id'],
+            "flow_group_id": flow['flow_group_id'],
+            "name":  flow['name'],
+            "project_id": flow['project_id'],
+            "is_schedule_active": flow['is_schedule_active']
+        }
+        flowList.append(f)
+    return flowList
 
+@task
+def listifyFlowRuns(all_Flows: object) -> list:
+    flowRuns = []
+    for flow_run in all_Flows['data']['flow_run']:
+        f = {
+            "id": flow_run['id'],
+            "name":  flow_run['name'],
+            "state": flow_run['state']
+        }
+        flowRuns.append(f)
+    return flowRuns
+
+# Returns all active flows in the listed project_id
 @task
 def queryFlowsByProject(project_id: str) -> list: 
 
     variables = {
-        "projectId": projectId
+        "project_id": project_id
     }
 
     flow_by_project_query = """
-        query Flows($projectId: uuid!): {
-        flow(where: project_id: {_eq: $projectId}): {
+        query Flows ($project_id: uuid!){
+        flow (where: {project_id: {_eq: $project_id}}) {
             id,
             flow_group_id,
             name,
@@ -95,35 +125,89 @@ def queryFlowsByProject(project_id: str) -> list:
         }
     """
     r = client.graphql(query=flow_by_project_query, variables=variables)
-    
-    return listifyFlows(r)
+    projectFlows = listifyFlows.run(r)
+
+    return projectFlows
 
 @task
-def listifyFlows(all_Flows: object) -> list:
-    flowList = []
-    for flow in all_Flows['data']['project']:
-        f = {
-            "id": flow['id'],
-            "flow_group_id": flow['flow_group_id'],
-            "name":  flow['name'],
-            "project_id": flow['project_id'],
-            "is_schedule_active": flow['is_schedule_active']
+def queryFlowRunTotalByProject(project_id: str) -> list: 
+
+    variables = {
+        "project_id": project_id
+    }
+
+    query = """
+    query TotalFlowRuns($project_id: uuid) {
+        flow_run(where: {flow: {project_id: {_eq: $project_id}}}) {
+            id,
+            name,
+            state
         }
-        flowList.append(f)
-    return flowList, len(flowList)
+    }
+    """
+    r = client.graphql(query=query, variables=variables)
+    
+    projectList = listifyFlowRuns.run(r)
+    return projectList
 
 @task
-def exportAllFlows():
-    # Updates projectTotal metrics with the label and value of each project queried
-    flowList, flow_count = queryAllFlows.run()
-    flowTotal.set(flow_count)
+def queryFlowRunSuccessByProject(project_id: str) -> list: 
+    variables = {
+        "project_id": project_id
+    }
 
-    for project in queryAllProjects.run():
+    query = """
+    query TotalFlowRuns($project_id: uuid) {
+        flow_run(where: {flow: {project_id: {_eq: $project_id}}, state: {_eq: "Success"}}) {
+            id,
+            name,
+            state
+        }
+    }
+    """
+    r = client.graphql(query=query, variables=variables)
+    
+    projectList = listifyFlowRuns.run(r)
+    return projectList
 
-        flowTotal.labels(project)
+
+
+@task
+# Updates projectTotal metrics with the label and value of each project queried
+def exportAllProjects(allProjects):
+    count = 0 
+    for project in allProjects:
         projectTotal.labels(project['name']).set(1)
-flowTotal.set()
-flowRunTotal.With(prometheus.Labels{"project_id": project.ID, "project_name": project.Name}).Set(respFlowsUpcoming.len())
+        count += 1
+    projectNumber.set(len(allProjects)) 
+
+
+# Sets and exports all flow totals across all projects
+@task 
+def exportAllFlows():
+    flowTotal.set(queryAllFlows.run())
+
+
+#Updates projectTotal metrics with the label and value of each project queried
+@task
+def exportFlowsByProject(allProjects):
+    for project in allProjects:
+        project_Flows = queryFlowsByProject.run(project['id'])
+        flowProjectTotal.labels(project['id'], project['name']).set(len(project_Flows))
+
+
+@task
+def exportFlowRunTotal(allProjects):
+    for project in allProjects:
+        project_Flows = queryFlowRunTotalByProject.run(project['id'])
+        flowRunTotal.labels(project['id'], project['name']).set(len(project_Flows))
+
+@task
+def exportFlowRunSuccess(allProjects):
+    for project in allProjects:
+        project_Flows = queryFlowRunSuccessByProject.run(project['id'])
+        flowRunTotalSuccess.labels(project['id'], project['name']).set(len(project_Flows))
+
 
 if __name__ == '__main__':
 
