100b53ae1cb40882bbc064207f94f047b4d44eaf
Emil Christensen <emil@prefect.io>
2022-08-24 11:46:05 -0400

    Initial terraform implementation of Prefect 2 agent on ECS

---
+++ devops/infrastructure-as-code/aws/tf-prefect2-ecs-agent/main.tf
@@ -0,0 +1,128 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_secretsmanager_secret" "prefect_api_key" {
+  name = "prefect-api-key"
+}
+
+resource "aws_secretsmanager_secret_version" "prefect_api_key_version" {
+  secret_id     = aws_secretsmanager_secret.prefect_api_key.id
+  secret_string = var.prefect_api_key
+}
+
+resource "aws_iam_role" "prefect_agent_execution_role" {
+  name = "prefect-agent-execution-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  inline_policy {
+    name = "ssm-allow-read-prefect-api-key"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action = [
+            "kms:Decrypt",
+            "secretsmanager:GetSecretValue",
+            "ssm:GetParameters"
+          ]
+          Effect = "Allow"
+          Resource = [
+            aws_secretsmanager_secret.prefect_api_key.arn
+          ]
+        }
+      ]
+    })
+  }
+
+  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"]
+}
+
+resource "aws_cloudwatch_log_group" "prefect_agent_log_group" {
+  name              = "prefect-agent-log-group"
+  retention_in_days = 30
+}
+
+resource "aws_ecs_cluster" "prefect_agent_cluster" {
+  name = "prefect-agent"
+}
+
+resource "aws_ecs_cluster_capacity_providers" "prefect_agent_cluster_capacity_providers" {
+  cluster_name       = aws_ecs_cluster.prefect_agent_cluster.name
+  capacity_providers = ["FARGATE"]
+}
+
+resource "aws_ecs_task_definition" "prefect_agent_task_definition" {
+  family = "prefect-agent"
+  cpu    = var.agent_cpu
+  memory = var.agent_memory
+
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+
+  container_definitions = jsonencode([
+    {
+      name    = "prefect-agent"
+      image   = var.agent_image
+      command = ["prefect", "agent", "start", "-q", var.agent_queue_name]
+      cpu     = var.agent_cpu
+      memory  = var.agent_memory
+      environment = [
+        {
+          name  = "PREFECT_API_URL"
+          value = "https://api.prefect.cloud/api/accounts/${var.prefect_account_id}/workspaces/${var.prefect_workspace_id}"
+        }
+      ]
+      secrets = [
+        {
+          name      = "PREFECT_API_KEY"
+          valueFrom = aws_secretsmanager_secret.prefect_api_key.arn
+        }
+      ]
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.prefect_agent_log_group.name
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "prefect-agent"
+        }
+      }
+    }
+  ])
+  execution_role_arn = aws_iam_role.prefect_agent_execution_role.arn
+  //   task_role_arn = ""
+}
+
+resource "aws_ecs_service" "prefect_agent_service" {
+  name          = "prefect-agent"
+  cluster       = aws_ecs_cluster.prefect_agent_cluster.id
+  desired_count = var.agent_desired_count
+  launch_type   = "FARGATE"
+  network_configuration {
+    assign_public_ip = true
+    subnets          = var.agent_subnets
+  }
+  task_definition = aws_ecs_task_definition.prefect_agent_task_definition.arn
+
+}
---
+++ devops/infrastructure-as-code/aws/tf-prefect2-ecs-agent/vars.tf
@@ -0,0 +1,56 @@
+variable "aws_region" {
+  description = "AWS region to place resources in"
+  default     = "us-east-1"
+  type        = string
+}
+
+variable "agent_cpu" {
+  description = "CPU units to allocate to agent"
+  default     = 1024
+  type        = number
+}
+
+variable "agent_desired_count" {
+  description = "Number of agents to run"
+  default     = 1
+  type        = number
+}
+
+variable "agent_image" {
+  description = "Container image for the agent"
+  default     = "prefecthq/prefect:2.2.0-python3.10"
+  type        = string
+}
+
+variable "agent_memory" {
+  description = "Memory units to allocate to agent"
+  default     = 2048
+  type        = number
+}
+
+variable "agent_subnets" {
+  description = "Subnets to place fargate tasks in"
+  type        = list(string)
+}
+
+variable "agent_queue_name" {
+  description = "Queue that agent should listen to"
+  default     = "default"
+  type        = string
+}
+
+variable "prefect_account_id" {
+  description = "Prefect cloud account ID"
+  type        = string
+}
+
+variable "prefect_workspace_id" {
+  description = "Prefect cloud workspace ID"
+  type        = string
+}
+
+variable "prefect_api_key" {
+  description = "Prefect cloud API key"
+  type        = string
+  sensitive   = true
+}
