a0abfea9d5b47184d37b1feaa870a8ab09c0af72
Taylor Curran <taylorcurranagtc@gmail.com>
2022-08-31 14:30:25 -0400

    Cleaning up comments

--- flows-advanced/etl/flow_using_context_and_client_for_flow_rescheduling.py
+++ flows-advanced/etl/flow_using_context_and_client_for_flow_rescheduling.py
@@ -1,33 +1,34 @@
-import asyncio
 from prefect.client import get_client
 from prefect.context import get_run_context
 from prefect.orion.schemas.states import Scheduled
 from prefect import flow, task, get_run_logger
 
-# -- Build a subflow to demonstrate get_run_context() and return_state argument.
+# -- Build a Subflow to Demonstrate get_run_context() and return_state Argument --
 @task
 def task_that_gives_context():
-    """For this task I will return the run context"""
     print("I am a task.")
     task_run_context_dict = get_run_context().task_run.dict()
     # check out the availble keys
     print(task_run_context_dict.keys())
+    return "Hello Result"
 
 
 @flow
 def flow_that_gives_context():
-    """For this subflow I will call with a `return_state=True` argument"""
-    task_that_gives_context(return_state=True)
+    
+    # check out available keys for task run context
+    task_state = task_that_gives_context(return_state=True)
+    task_result = task_state.result()
 
+    # check out the availble keys for flow run context
     flow_run_context_dict = get_run_context().flow_run.dict()
-    # check out the availble keys
     print(flow_run_context_dict.keys())
 
-    # Now we will raise an artificial error.
+    # Now we will raise an artificial error that will prompt us to rescheudle the parent flow.
     raise Exception('Deliberate Failure for Example.')
 
 
-# -- Build a Subflow to Add Scheduled Flow Runs to a Deployment
+# -- Build a Subflow to Add Scheduled Flow Runs to a Deployment --
 @task
 async def add_new_scheduled_run(depl_id, original_start_time, delta_hours=6): 
     """This task adds a scheduled flow run delta_hours from the expected start time of the current flow."""
@@ -48,13 +49,16 @@ def scheduling_flow(depl_id, original_start_time):
 @flow
 def main_flow():
 
+    # Run the Context Demo Sub-Flow
     flow_state = flow_that_gives_context(return_state=True)
-    # check out the availble context keys
+
+    # check out the availble state keys
     print(flow_state.dict().keys)
     
     # We'll use the state.is_completed method.
     print('In complete state?', flow_state.is_completed())
 
+    # Lets schedule this flow to run in a few hours from now if the subflow failed.
     if not flow_state.is_completed():
         # Use Context to Get Deployment ID and Scheduled Start Time
         depl_id = get_run_context().flow_run.dict()['deployment_id']
