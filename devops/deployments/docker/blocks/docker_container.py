2924dd794622083175cec816c4b28d78c7690f76
Emil Christensen <emil@prefect.io>
2022-11-18 10:06:36 -0500

    Fixes linting error

--- devops/deployments/docker/blocks/docker_container.py
+++ devops/deployments/docker/blocks/docker_container.py
@@ -5,7 +5,7 @@ def create_docker_container():
     block = DockerContainer(
         name="prefect-docker-example",
         image="prefect-docker-example:latest",
-        # We want to always use our local prefect-docker-example:latest, so we NEVER pull it
+        # We want to always use our local image, so we NEVER pull it
         image_pull_policy=ImagePullPolicy.NEVER,
     )
     block.save("prefect-docker-example", overwrite=True)
