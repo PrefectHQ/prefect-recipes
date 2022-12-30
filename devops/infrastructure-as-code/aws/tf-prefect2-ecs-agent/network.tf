176ad65ac1bd16148f1c83479f1d11f14c4f7fe1
gabcoyne <gabcoyne@gmail.com>
2022-12-30 09:05:28 -0600

    add sg to ecs

---
+++ devops/infrastructure-as-code/aws/tf-prefect2-ecs-agent/network.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "prefect_agent" {
+  name        = "ECSPrefectAgent"
+  description = "ECS Prefect Agent"
+}
+
+resource "aws_security_group_rule" "https_outbound" {
+  # S3 Gateway interfaces are implemented at the routing level which means we
+  # can avoid the metered billing of a VPC endpoint interface by allowing
+  # outbound traffic to the public IP ranges, which will be routed through
+  # the Gateway interface:
+  # https://docs.aws.amazon.com/AmazonS3/latest/userguide/privatelink-interface-endpoints.html
+  description       = "HTTPS outbound"
+  type              = "egress"
+  security_group_id = aws_security_group.prefect_agent.id
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
