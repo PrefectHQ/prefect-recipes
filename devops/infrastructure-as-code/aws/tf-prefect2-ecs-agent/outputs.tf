5adce3c0126d8fa54bf79bd12630d396afc38650
Emil Christensen <emil@prefect.io>
2023-01-17 16:16:51 -0500

    Adds additional output values

--- devops/infrastructure-as-code/aws/tf-prefect2-ecs-agent/outputs.tf
+++ devops/infrastructure-as-code/aws/tf-prefect2-ecs-agent/outputs.tf
@@ -1,3 +1,19 @@
 output "prefect_agent_service_id" {
   value = aws_ecs_service.prefect_agent_service.id
 }
+
+output "prefect_agent_execution_role_arn" {
+  value = aws_iam_role.prefect_agent_execution_role.arn
+}
+
+output "prefect_agent_task_role_arn" {
+  value = aws_iam_role.prefect_agent_task_role.arn
+}
+
+output "prefect_agent_security_group" {
+  value = aws_security_group.prefect_agent.id
+}
+
+output "prefect_agent_cluster" {
+  value = aws_ecs_cluster.prefect_agent_cluster.name
+}
