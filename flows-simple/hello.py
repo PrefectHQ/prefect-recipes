bdca024ce45d68b34897171535106fc429ded76e
nate nowack <thrast36@gmail.com>
2023-02-22 09:14:06 -0600

    add simple demo flow (#203)

---
+++ flows-simple/hello.py
@@ -0,0 +1,32 @@
+from prefect import flow
+from prefect.blocks.system import String
+
+
+@flow(log_prints=True)
+def hello(block_name: str = "name"):
+    """A simple flow that says hello.
+
+    Assumes a `String` block named "name" exists in the active
+    Prefect Cloud workspace.
+
+    Args:
+        block_name: The name of the block to load. Defaults to "name".
+
+    """
+    name = String.load(block_name)
+    print(f"Hello {name.value}!")
+
+    if "Expert" not in name.value:
+        print("You're about to become a Prefect expert!")
+        name.value = name.value + " the Prefect Expert"
+        print(f"Your name is now {name.value}!")
+        name.save("name", overwrite=True)
+    else:
+        print("You're already a Prefect expert!")
+        print(
+            "Check out some Prefect recipes: https://docs.prefect.io/recipes/recipes/"
+        )
+
+
+if __name__ == "__main__":
+    hello(block_name="name")
