a20b90109934419c7639f6e3575a87b0ea8a821d
James Bada Sopkin <69161193+Bada-S@users.noreply.github.com>
2023-05-09 09:25:45 -0500

    updated flow code

--- devops/infrastructure-as-code/azure/prefect-worker-on-aks/flow.py
+++ devops/infrastructure-as-code/azure/prefect-worker-on-aks/flow.py
@@ -1,16 +1,49 @@
-from prefect import flow, get_run_logger, task
+from prefect import task, flow
+from prefect import get_run_logger
+import pandas as pd
+import os
+from io import BytesIO
+
+from prefect_azure import AzureBlobStorageCredentials
+from prefect_azure.blob_storage import blob_storage_download
+
+# @task(log_prints=True)
+def load_from_azure():
+    connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+    print(connection_string)
+    blob_storage_credentials = AzureBlobStorageCredentials(
+        connection_string=connection_string
+    )
+
+    data = blob_storage_download(
+        blob="file.csv",
+        container="prefect-logs",
+        blob_storage_credentials=blob_storage_credentials,
+    )
+    return data
 
 
 @task
-def hello_world():
-    logger = get_run_logger()
-    logger.info("Hello world!")
+def read_file(data):
+    return pd.read_csv(BytesIO(data))
+
+
+@task(log_prints=True)
+def transform_pd(df):
+    results = [row["col1"] * row["col2"] for index, row in df.iterrows()]
+    print(results)
 
 
 @flow(log_prints=True)
-def myflow():
-    hello_world()
+def transform_flow():
+    logger = get_run_logger()
+
+    file = load_from_azure()
+    print(type(file))
+    df = read_file(file)
+
+    transform_pd(df)
 
 
 if __name__ == "__main__":
-    myflow()
+    transform_flow()
