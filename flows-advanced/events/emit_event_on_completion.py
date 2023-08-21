aa0db045f04a1d373383ba0f7bf83b084f324929
Nathan Nowack <thrast36@gmail.com>
2023-08-21 08:43:47 -0500

    add some on_completion examples

---
+++ flows-advanced/events/emit_event_on_completion.py
@@ -0,0 +1,37 @@
+from prefect import flow
+from prefect.client.schemas.objects import Flow, FlowRun
+from prefect.events import emit_event
+from prefect.filesystems import GCS
+from prefect.states import State
+
+
+def emit_on_complete(flow: Flow, flow_run: FlowRun, state: State):
+    print(
+        f"hello from {flow_run.name}'s completion hook |"
+        f" the return value was {(r := state.result())!r}"
+    )
+    emit_event(
+        event="prefect.result.produced",  # this is an arbitrary event name
+        resource={
+            "prefect.resource.id": (
+                f"prefect.result.{flow_run.deployment_id}.{flow_run.id}"
+            )
+        },
+        payload={"result": r},
+    )
+
+
+# prefect deploy emit_event_on_completion.py:foobar_event
+@flow(
+    persist_result=True,
+    result_storage=GCS.load("my-result-storage"),
+    on_completion=[emit_on_complete],
+)
+def foobar_event() -> str:
+    return "foobar"
+
+
+# prefect deploy emit_event_on_completion.py:event_triggered_flow
+@flow(log_prints=True)
+def event_triggered_flow(prev_result: str) -> str:
+    print(f"got {prev_result=!r}")
--- flows-advanced/events/run_deployment_on_completion.py
+++ flows-advanced/events/run_deployment_on_completion.py
@@ -2,25 +2,26 @@ from prefect import flow
 from prefect.deployments import run_deployment
 from prefect.filesystems import GCS
 
+
 def trigger_on_complete(flow, flow_run, state):
     print(
         f"hello from {flow_run.name}'s completion hook |"
         f" the return value was {(r := state.result())!r}"
     )
-    run_deployment(
-        name="triggered-flow/triggered",
-        parameters=dict(prev_result=r)
-    )
+    run_deployment(name="triggered-flow/triggered", parameters=dict(prev_result=r))
 
+
+# prefect deploy run_deployment_on_completion.py:foobar
 @flow(
     persist_result=True,
     result_storage=GCS.load("my-result-storage"),
-    on_completion=[trigger_on_complete]
+    on_completion=[trigger_on_complete],
 )
 def foobar() -> str:
     return "foobar"
 
 
+# prefect deploy run_deployment_on_completion.py:triggered_flow
 @flow(log_prints=True)
 def triggered_flow(prev_result: str) -> str:
     print(f"got {prev_result=!r}")
