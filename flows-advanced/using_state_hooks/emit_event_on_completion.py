006d3dfc759d8a81e341d2ae3e03eafb35f70716
nate nowack <thrast36@gmail.com>
2023-09-15 11:29:19 -0500

    docstring

--- flows-advanced/using_state_hooks/emit_event_on_completion.py
+++ flows-advanced/using_state_hooks/emit_event_on_completion.py
@@ -11,6 +11,7 @@ from prefect.states import State
 
 
 def emit_on_complete(flow: Flow, flow_run: FlowRun, state: State):
+    """State change hook that will run upon the `flow_run` entering a `Completed` state"""
     print(
         f"hello from {flow_run.name}'s completion hook |"
         f" the return value was {(r := state.result())!r}"
