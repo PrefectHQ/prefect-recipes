dc3a601fbcf01f0a66c6caf30111715415bec0be
lucianosrp <66913960+lucianosrp@users.noreply.github.com>
2024-03-17 19:27:23 +0100

    Add dot-env-secrets recipe (#265)
    * Adds dot-env-secrets recipe
    
    * Update flows-advanced/dot-env-secrets/README.md
    
    Co-authored-by: nate nowack <nate@prefect.io>
    
    * Move recipe
    
    ---------
    
    Co-authored-by: nate nowack <nate@prefect.io>

---
+++ configuration/dot-env-secrets/main.py
@@ -0,0 +1,39 @@
+from typing import Any
+
+from dotenv import dotenv_values
+from prefect.blocks.system import Secret
+
+
+def rename_keys(d: dict[str, Any]) -> dict[str, Any]:
+    """
+    Variable names need to be renamed in order to be valid Block names.
+
+    Examples:
+    --------
+    >>> original = {'TEST_KEY':"test_value", "test-key-valid":"test_value"}
+    >>> rename_keys(original)
+    {'test-key': 'test_value', 'test-key-valid': 'test_value'}
+
+    """
+    n = {}
+    for k, v in d.items():
+        n[k.replace("_", "-").casefold()] = v
+    return n
+
+
+def load_secrets(fp: str, overwrite: bool = False):
+    original_values = dotenv_values(fp, verbose=True)
+
+    if original_values == {}:
+        raise IOError("File not found or empty")
+
+    d = rename_keys(original_values)
+    for k, v in d.items():
+        secret = Secret(value=v)
+        uuid = secret.save(name=k, overwrite=overwrite)
+        print(k, uuid)
+
+
+if __name__ == "__main__":
+    env_file = "flows-advanced/configuration/dot-env-secrets/sample-dot-env"
+    load_secrets(env_file)
